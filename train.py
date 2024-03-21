24f8d6fcc7486ba8f670367085dda74ff528601c
NaraineSurya <narayanansuryatcr@gmail.com>
2024-03-21 08:00:07 +0530

    building training loop

--- train.py
+++ train.py
@@ -14,4 +14,51 @@ from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
 from model import Discriminator, Generator, initialize_weights
 
+device = torch.device("cude" if torch.cuda.is_available else "cpu")
+LEARNING_RATE = 2e-4
+BATCH_SIZE = 128
+IMG_SIZE = 64
+CHANNELS_IMG = 1 
+Z_DIM = 100
+NUM_EPOCH = 5
+FEATURES_DISC = 64
+FEATURES_GEN = 64
+
+transforms = transforms.Compose(
+    [
+        transforms.Resize(IMG_SIZE),
+        transforms.ToTensor(),
+        transforms.Normalize( [0.5 for _ in range(CHANNELS_IMG) ], [0.5 for _ in range(CHANNELS_IMG)] ),
+    ]
+)
+
+dataset = datasets.MNIST(root="dataset/", train=True, transform=transforms, download=True)
+loader = DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True)
+gen = Generator(Z_DIM, CHANNELS_IMG, FEATURES_GEN).to(device)
+disc = Discriminator(CHANNELS_IMG, FEATURES_GEN).to(device)
+
+initialize_weights(gen)
+initialize_weights(disc)
+
+opt_gen = optim.Adam(gen.parameters(), lr=LEARNING_RATE, betas=(0.5,0.999))
+opt_disc = optim.Adam(disc.parameters(), lr=LEARNING_RATE, betas=(0.5,0.999))
+criterion = nn.BCELoss()
+fixed_noise =  torch.randn(32, Z_DIM, 1, 1).to(device)
+
+writer_real = SummaryWriter(f"logs/real")
+writer_fake = SummaryWriter(f"logs/fake")
+step = 0
+
+gen.train()
+disc.train()
+
+for epoch in range(NUM_EPOCH):
+    for batch_idx, (real, _) in enumerate(loader):
+        real = real.to(device)
+        noise = torch.randn((BATCH_SIZE, Z_DIM, 1, 1)).to(device)
+        
+
+
+
+
 
