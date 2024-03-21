8ceef23b505f9521088a3b5741b1c9a6b98e2ad9
NaraineSurya <narayanansuryatcr@gmail.com>
2024-03-21 07:37:47 +0530

    model imported

--- train.py
+++ train.py
@@ -1,5 +1,17 @@
 '''
-Training DCGAN network on MNIST data with Discriminator
+Training DCGAN network on Celebrity data with Discriminator
 and Generator imported from model.py
 
 '''
+
+import torch 
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+from model import Discriminator, Generator, initialize_weights
+
+
